9a5a48f56a57bc10271a5577c2666a37e8a74e7d
peteredwards <p.l.edwards@leeds.ac.uk>
2013-04-18 17:36:37 +0100

    added settings page for IP ban list (untested)

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 uol-private-blog
 ================
 
-Makes a blog on a multisite network private by limiting access to logged-in users, or users within a given IP range
+Makes a blog private by limiting access to logged-in users, or users within a given IP range.
+
+		$ips = array(
+			"129.11",
+			"10.11.40",
+			"10.11.41",
+			"194.82.12",
+			"194.82.13",
+			"194.82.14",
+			"194.82.15",
+			"194.80.232",
+			"194.80.233",
+			"194.80.234",
+			"194.80.235"
+		);
--- uol-private-blog.php
+++ uol-private-blog.php
@@ -13,46 +13,134 @@ if (!class_exists("uol_privacy")) :
  */
 class uol_privacy
 {
-        public static function register()
-        {
-            add_action( 'wp', array('uol_privacy', 'force_member_login_init') );
-        }
+	public static function register()
+	{
+		if (self::is_sitewide()) {
+	        /* add menu to Network admin page */
+	        add_action('network_admin_menu', array('uol_privacy', 'addAdminMenu'));
+	    } else {
+	    	add_action('admin_menu', array('uol_privacy', 'addAdminMenu'));
+	    }
+		add_action( 'wp', array('uol_privacy', 'force_member_login_init') );
+		
+	}
 
-        function force_member_login_init() 
+	function force_member_login_init() 
+	{
+		/* If the user is logged in, then abort */
+		if ( current_user_can('read') ) return;
+
+		/* if the user appears to have a valid IP address, abort */
+		$ipstr = self::get_option();
+		if ( !empty($ipstr) ) {
+			$ips = explode("|", $ipstr);
+			$ip_is_allowed = false;
+			foreach ($ips as $ip) {
+				if (strpos($_SERVER["REMOTE_ADDR"], $ip) === 0) {
+					$ip_is_allowed = true;
+				}
+			}
+			if ($ip_is_allowed) return;
+		}
+
+		/* This is an array of pages that will be EXCLUDED from being blocked */
+		$exclusions = array(
+			'wp-login.php',
+			'wp-cron.php', // Just incase
+			'wp-trackback.php',
+			'xmlrpc.php'
+		);
+
+		/* If the current script name is in the exclusion list, abort */
+		if ( in_array( basename($_SERVER['PHP_SELF']), $exclusions) ) return;
+
+		/* Still here? Okay, then redirect to the login form */
+		auth_redirect();
+	}
+
+	/**
+	 * uses add_submenu_page to add a menu item to the Settings menu (on the network admin dashboard or the blog dashboard)
+	 */
+    public static function addAdminMenu()
     {
-        /* If the user is logged in, then abort */
-        if ( current_user_can('read') ) return;
-                /* if the user appears to be on the campus network, then abort */
-                $ips = array(
-                    "129.11",
-                        "194.82.12",
-                        "194.82.13",
-                        "194.82.14",
-                        "194.82.15",
-                        "194.80.232",
-                        "194.80.233",
-                        "194.80.234",
-                        "194.80.235"
-                );
-                $is_on_campus = false;
-                foreach ($ips as $ip) {
-                    if (strpos($_SERVER["REMOTE_ADDR"], $ip) === 0) {
-                            $is_on_campus = true;
-                        }
-                }
-                if ($is_on_campus) return;
-        /* This is an array of pages that will be EXCLUDED from being blocked */
-        $exclusions = array(
-            'wp-login.php',
-            'wp-cron.php', // Just incase
-            'wp-trackback.php',
-            'xmlrpc.php'
-        );
-        /* If the current script name is in the exclusion list, abort */
-        if ( in_array( basename($_SERVER['PHP_SELF']), $exclusions) ) return;
-        /* Still here? Okay, then redirect to the login form */
-        auth_redirect();
+		/* adjust capability according to activation status of plugin */
+		$cap = (self::is_sitewide())? 'manage_network_options': 'manage_options';
+    	add_submenu_page( 'settings.php', 'Privacy', 'Privacy', $cap, 'uol-privacy', array('uol_privacy', 'get_admin_page'));
+    }
+
+    /**
+     * options page
+     */
+    public static function get_admin_page()
+    {
+        /* check that the user has the required capability */ 
+		$cap = (self::is_sitewide())? 'manage_network_options': 'manage_options';
+        if (!current_user_can($cap))
+        {
+            wp_die( __('You do not have sufficient permissions to access this page.') );
         }
+        /* save options */
+        if (isset($_POST["ip_list"])) {
+        	if ( wp_verify_nonce( $_POST['ip_allow_list'], 'ip_allow_list_nonce' )) {
+	        	if (trim($_POST["ip_list"]) != '') {
+		        	$ips = explode("\n", $_POST["ip_list"]);
+		        	$valid_ips = array();
+	    	    	foreach ($ips as $ip) {
+	    	    		$isValid = true;
+	    	    		/* make sure it is only numbers and dots */
+	    	    		$ip = trim(preg_replace('/[^0-9\.]*/', '', $ip), ". \t\n\r\0\x0B");
+	    	    		/* split into quads */
+	    	    		$quads = explode(".", $ip);
+	    	    		/* test each quad */
+	    	    		foreach ($quads as $quad) {
+	    	    			if (!preg_match('/(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/', $quad)) {
+	    	    				$isValid = false;
+	    	    			}
+	    	    		}
+	    	    		if ($isValid) {
+	    	    			$valid_ips[] = $ip;
+	    	    		}
+	    	    	}
+	    	    	if (count($valid_ips)) {
+	    	    		$ipstr = implode("|", $valid_ips);
+	    	    		if (self::is_sitewide()) {
+	    	    			update_site_option('uol_privacy', $ipstr);
+	    	    		} else {
+	    	    			update_option('uol_privacy', $ipstr);
+	    	    		}
+	    	    	}
+	    	    }
+	    	}
+	    }
+	    $ips = self::get_option();
+	    printf('<div class="wrap"><div id="icon-settings" class="icon32"></div><h2>%s</h2><form name="custom-login-page-settings-form" method="post" action="">', __('Privacy settings'));
+        printf('<input type="hidden" name="ip_allow_list" id="ip_allow_list" value="%s" />', wp_create_nonce('ip_allow_list_nonce'));
+        printf('<p>%s</p><p><textarea name="ip_allow_list" id="ip_allow_list">%s</textarea></p>', __('Input a list of IP addresses, separated by linebreaks, to allow onto the site without the user having to log in. Partial IP addresses are OK'), explode("|", $ips));
+        printf('<p><input type="submit" name="submit" class="button-primary" value="%s" /></p></form></div>', __('Save Changes'));
+    }
+
+    public static function is_sitewide()
+    {
+    	return (is_multisite() && is_plugin_active_for_network(plugin_basename(__FILE__)));
+    }
+
+    /**
+	 * gets default plugin options
+	 */
+    public static function get_option() 
+    {
+    	if (self::is_sitewide()) {
+    		$option = get_site_option('uol_privacy');
+    	} else {
+    		$option = get_option('uol_privacy');
+    	}
+    	if ($option === false) {
+    		return '';
+    	} else {
+	        return $option;
+	    }
+    }
+
 }
 uol_privacy::register();
 endif;
